88921eba2cc8ef632caf362bb9d63019f510a4a9
Сергей С. Зябликов <sergey.zyablikov@arealidea.ru>
2024-02-07 14:38:54 +0300

    fetch: setup ready

---
+++ apps/apigeteway/src/users/users.service.ts
@@ -0,0 +1,58 @@
+import { Inject, Injectable, OnModuleInit } from '@nestjs/common';
+import {
+  AUTH_PACKAGE_NAME,
+  CreateUserDto,
+  PaginationDto,
+  UpdateUserDto,
+  USERS_SERVICE_NAME,
+  UsersServiceClient,
+} from '@app/common';
+import { ClientGrpc } from '@nestjs/microservices';
+import { ReplaySubject } from 'rxjs';
+
+@Injectable()
+export class UsersService implements OnModuleInit {
+  private userService: UsersServiceClient;
+
+  constructor(@Inject(AUTH_PACKAGE_NAME) private client: ClientGrpc) {}
+  onModuleInit() {
+    this.userService =
+      this.client.getService<UsersServiceClient>(USERS_SERVICE_NAME);
+  }
+
+  create(createUserDto: CreateUserDto) {
+    return this.userService.createUser(createUserDto);
+  }
+
+  findAll() {
+    return this.userService.findAllUsers({});
+  }
+
+  findOne(id: string) {
+    return this.userService.findOneUser({ id });
+  }
+
+  update(id: string, updateUserDto: UpdateUserDto) {
+    return this.userService.updateUser({ id, ...updateUserDto });
+  }
+
+  remove(id: string) {
+    return this.userService.removeUser({ id });
+  }
+
+  emailUsers() {
+    const users = new ReplaySubject<PaginationDto>();
+    users.next({ page: 0, skip: 25 });
+    users.next({ page: 1, skip: 25 });
+    users.next({ page: 2, skip: 25 });
+
+    users.complete();
+
+    let count = 1;
+
+    this.userService.queryUsers(users).subscribe((users) => {
+      console.log('Chunk', count, users);
+      count += 1;
+    });
+  }
+}
